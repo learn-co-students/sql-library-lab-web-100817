ed65020068283047ae7b274e69668aaa2c2978b5
Gonzodamus <Dick@TheDickWard.com>
2017-10-15 15:55:27 -0400

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id TEXT,
+  subgenre_id TEXT );
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INT,
+  series_id INT
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INT,
+  series_id INT
+);
+
+CREATE TABLE character_books(
+  id INTEGER PRIMARY KEY,
+  character_id INT,
+  book_id INT
+);
